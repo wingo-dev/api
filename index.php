432f018b77f994b5ed033f664c55922ad24d683c
= <rukshanlancer@gmail.com>
2023-02-23 00:33:39 +0800

    github starred count

--- index.php
+++ index.php
@@ -2,17 +2,26 @@
 
 $ch = curl_init();
 
-
+$headers = [
+    "Authorization: Bearer API key"
+];
 curl_setopt_array(
     $ch,
     [
-        CURLOPT_URL => "",
-        CURLOPT_RETURNTRANSFER => true
+        CURLOPT_URL => "https://api.github.com/repos/wingo-dev/Delos",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_USERAGENT => "wingo-dev"
     ]
 );
 
 $response = curl_exec($ch);
 
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+echo $status_code;
+
+echo $response;
+
 
 curl_close($ch);
