b43b1a4f3f451e2a40c7b2bc200b49ff75b0915b
= <rukshanlancer@gmail.com>
2023-02-24 16:38:39 +0800

    REST api by using .htaccess

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
 
-// GUZZLE usage
-
-require __DIR__ . "/vendor/autoload.php";
-
-$client = new GuzzleHttp\Client;
-
-$response = $client->request("GET", "https://api.github.com/user/repos", [
-    "headers" => [
-        "Authorization" => "token Your_token",
-        "user-Agent" => "wingo-dev"
-    ]
-]);
-
-
-echo $response->getStausCode() . "\n";
-
-echo $response->getHeader("content-type")[0] . "\n";
-
-echo substr($response->getBody(), 0, 200) . "\n";
+echo "hello";
