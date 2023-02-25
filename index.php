e4ae65d993efe65679b3bbc72039728e6e3fdea3
= <rukshanlancer@gmail.com>
2023-02-25 18:34:46 +0800

    controller class for api

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
-echo "hello";
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+
+$id = $parts[3] ?? null;
+
+// echo $resource, ", ", $id;
+
+// echo $_SERVER["REQUEST_METHOD"], "\n";
+
+if ($resource != "tasks") {
+    // header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+    http_response_code(404);
+    exit;
+}
+
+
+
+
+require dirname(__DIR__) . "/api/src/controller.php";
+
+$controller = new TaskController;
+
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
