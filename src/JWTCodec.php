a41ab8dae92a2313c73cc8d5fb2790815f5dd309
= <rukshanlancer@gmail.com>
2023-03-04 18:00:26 +0800

    JWT token class creation

---
+++ src/JWTCodec.php
@@ -0,0 +1,76 @@
+<?php
+
+class JWTCodec
+{
+    public function encode(array $payload): string
+    {
+        $header = json_encode([
+            "typ" => "JWT",
+            "alg" => "HS256"
+        ]);
+        $header = $this->base64urlEncode($header);
+
+        $payload = json_encode($payload);
+        $payload = $this->base64urlEncode($payload);
+
+        $signature = hash_hmac(
+            "sha256",
+            $header . "." . $payload,
+            "5A7134743777217A25432646294A404E635266556A586E3272357538782F413F",
+            true
+        );
+        $signature = $this->base64urlEncode($signature);
+
+        return $header . "." . $payload . "." . $signature;
+    }
+
+    public function decode(string $token): array
+    {
+        if (preg_match(
+            "/^(?<header>.+)\.(?<payload>.+)\.(?<signature>.+)$/",
+            $token,
+            $matches
+        ) !== 1) {
+
+            throw new InvalidArgumentException("invalid token format");
+        }
+
+        $signature = hash_hmac(
+            "sha256",
+            $matches["header"] . "." . $matches["payload"],
+            "5A7134743777217A25432646294A404E635266556A586E3272357538782F413F",
+            true
+        );
+
+        $signature_from_token = $this->base64urlDecode($matches["signature"]);
+
+        if (!hash_equals($signature, $signature_from_token)) {
+
+            throw new Exception("signature doesn't match");
+        }
+
+        $payload = json_decode($this->base64urlDecode($matches["payload"]), true);
+
+        return $payload;
+    }
+
+    private function base64urlEncode(string $text): string
+    {
+        return str_replace(
+            ["+", "/", "="],
+            ["-", "_", ""],
+            base64_encode($text)
+        );
+    }
+
+    private function base64urlDecode(string $text): string
+    {
+        return base64_decode(
+            str_replace(
+                ["-", "_"],
+                ["+", "/"],
+                $text
+            )
+        );
+    }
+}
